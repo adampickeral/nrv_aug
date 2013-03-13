f38d79a96635e49cf1a85a827a10b640a5ac8437
adampickeral <adam.pickeral@rackspace.com>
2013-03-13 05:54:30 -0400

    subscribe to mailing list

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var request = require('request');
+var qs = require('querystring');
+var config = require('./config');
 
 var server = express();
 
@@ -9,6 +12,35 @@ server.set('views', __dirname + '/views');
 
 server.get('/', function (req, res) {
   res.render('index.ejs');
+});
+
+server.post('/mailingList', function (req, res) {
+  var name, email, options, data, body;
+
+  data = '';
+  req.on('data', function(chunk) {
+    data += chunk;
+  });
+
+  req.on('end', function() {
+    body = qs.parse(data);
+    options = {
+      url: config.membersList,
+      auth: {
+        user: 'api',
+        pass: config.key
+      },
+      form: {
+        subscribed: true,
+        address: body.email,
+        name: body.name
+      }
+    };
+
+    request.post(options, function (error, response, body) {
+      res.status(response.statusCode).send(body);
+    });
+  });
 });
 
 server.listen(3000);
