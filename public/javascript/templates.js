ad147fb77b485292e9f3b41aa16f899ea583dc0b
adampickeral <adam.pickeral@rackspace.com>
2013-05-30 05:35:02 -0400

    agile testing book resource

--- public/javascript/templates.js
+++ public/javascript/templates.js
@@ -59,16 +59,22 @@ var resources =
       '<h2>Books</h2>' +
       '<books>' +
         '<book class="first">' +
-          '<img src="/images/art_of_agile_development.gif">' +
+          '<div class="image"><img src="/images/art_of_agile_development.gif"></div>' +
           '<h3>The Art of Agile Development</h3>' +
-          '<author>James Shore</author>' +
+          '<author>James Shore, Shane Warden</author>' +
           '<synopsis>Shore provides a detailed look at eXtreme Programming, explaining topics such as team roles, practices, work environment, and even why one would choose to be agile and utilize XP within their development process. An excellent resource for new and experienced agile practitioners.</synopsis>' +
         '</book>' +
         '<book>' +
-          '<img src="/images/user_stories_applied.jpg">' +
+          '<div class="image"><img src="/images/user_stories_applied.jpg"></div>' +
           '<h3>User Stories Applied</h3>' +
           '<author>Mike Cohn</author>' +
           '<synopsis>Cohn covers the ins and outs of user stories, discussing what they are, what makes a good user story, and also what makes a bad user story. He also explains how to use user stories when you don\'t have access to a user, as well as how to prioritize and estimate user stories.</synopsis>' +
+        '</book>' +
+        '<book>' +
+          '<div class="image"><img src="/images/agile_testing.jpg"></div>' +
+          '<h3>Agile Testing: A Practical Guide for Testers and Agile Teams</h3>' +
+          '<author>Lisa Crispin, Janet Gregory</author>' +
+          '<synopsis>This books explains the differences between testing in a traditional development process and testing in an agile process. The role of the tester changes from a reactive to a proactive one, and the approach to testing shifts from developing a test plan to a test strategy. The agile testing matrix is used to explain all the different types of testing needed on an agile project.</synopsis>' +
         '</book>' +
       '</books>' +
     '</div>' +
