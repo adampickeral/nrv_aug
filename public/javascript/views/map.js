318dca510ca9d19a6d606968a766c3840bbf8c0a
adampickeral <adam.pickeral@rackspace.com>
2014-12-31 16:11:33 -0500

    January 2015 meeting

--- public/javascript/views/map.js
+++ public/javascript/views/map.js
@@ -12,7 +12,7 @@ nrvaug.views.Map.prototype.render = function () {
   this.map_ = nrvaug.views.Map.createGoogleMap($('#map-canvas')[0], mapOptions)
   geocoder = nrvaug.views.Map.createGoogleGeocoder();
   geocoder.geocode(
-    {'address': ' Interactive Achievement Inc 601 Campbell Ave SW Roanoke, VA 24016'},
+    {'address': '432 North Main Street, Suite 200, Blacksburg, VA 24060'},
     this.setLocation.bind(this)
   );
 };
@@ -28,7 +28,7 @@ nrvaug.views.Map.prototype.setLocation = function (results, status) {
       title: 'Modea'
     });
     infoWindow = nrvaug.views.Map.createGoogleInfoWindow(
-      '<p>Interactive Achievement Inc</p><p>601 Campbell Ave SW</p><p>Roanoke, VA 24016</p>'
+      '<p>TechPad (located above PK\'s)</p><p>432 North Main Street, Suite 200</p><p>Blacksburg, VA 24060</p>'
     );
     google.maps.event.addListener(marker, 'click', function() {
       infoWindow.open(this.map_, marker);
